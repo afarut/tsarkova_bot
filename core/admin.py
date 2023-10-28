6c6ee15647046843dd38bbe5e552c87b1b796957
afarut <renat030205@ya.ru>
2023-10-28 13:58:52 +0300

    admin: m2m in other model

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
+from django.db import models
 from .models import Question, Answer, Filter, TelegramUser
-# Register your models here.
-admin.site.register(Question)
+from django.forms import CheckboxSelectMultiple
+
+
 admin.site.register(Answer)
-admin.site.register(Filter)
 admin.site.register(TelegramUser)
+
+
+class FilterInline(admin.TabularInline):
+    model = Question.filters.through
+    extra = 0
+    can_delete = False
+    show_change_link = True
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = [FilterInline]
+
+
+@admin.register(Filter)
+class FilterAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
